e0198a02c7bc4f7417c22c61b2d3e1be01e705ab
Katie Weber <k.weber0893@gmail.com>
2018-07-30 10:37:10 -0400

    Omniauth

---
+++ db/migrate/20180730142838_create_users.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :uid
+
+      t.timestamps null: false
+    end
+  end
+end
